7209ac6c4c90c60de77c8bc5b902afca8f114526
ChiminhTT <0ho.maxence0@gmail.com>
2016-12-13 04:29:10 +0100

    Clang Format

--- src/vm/DalvikVersion.h
+++ src/vm/DalvikVersion.h
@@ -23,15 +23,15 @@
 /*
  * The version we show to tourists.
  */
-#define DALVIK_MAJOR_VERSION    1
-#define DALVIK_MINOR_VERSION    2
-#define DALVIK_BUG_VERSION      0
+#define DALVIK_MAJOR_VERSION 1
+#define DALVIK_MINOR_VERSION 2
+#define DALVIK_BUG_VERSION 0
 
 /*
  * VM build number.  This must change whenever something that affects the
  * way classes load changes, e.g. field ordering or vtable layout.  Changing
  * this guarantees that the optimized form of the DEX file is regenerated.
  */
-#define DALVIK_VM_BUILD         19
+#define DALVIK_VM_BUILD 19
 
 #endif /*_DALVIK_VERSION*/
